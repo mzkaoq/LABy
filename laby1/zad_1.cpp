69aab2e5e3fadade761ba2c814af01a091dacca9
mzkaoq <mzkaoq333@gmail.com>
2020-10-26 11:49:15 +0100

    zad 1

---
+++ laby1/zad_1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    int *numbers = new int[argc - 1];
+
+    for (int i = 1; i < argc; i++) {
+        numbers[i - 1] = stoi(argv[i]);
+    }
+
+    int temp, j;
+    for (int i = 1; i < argc-1; i++)
+    {
+
+        temp = numbers[i];
+        j = i - 1;
+
+        while (j >= 0 && numbers[j] > temp)
+        {
+            numbers[j + 1] = numbers[j];
+            --j;
+        }
+        numbers[j + 1] = temp;
+    }
+    cout << "\n";
+    for (int i = 0; i < argc-1; ++i)
+        cout << numbers[i] << " ";
+
+    delete[] numbers;
+
+    return 0;
+}
