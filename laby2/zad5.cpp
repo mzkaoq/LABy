b2a7fe47dd1efde27746bc250972bae3cf4bc8b3
mzkaoq <mzkaoq333@gmail.com>
2020-11-04 00:19:18 +0100

    zad 5, zamiana dwóch wartosci przy użyciu swap, jak teraz patrze to zadania 1-4 powinny byc chyba zrobione bez swapa tylko z pomocnicza zmienna :)

---
+++ laby2/zad5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void funckja(int *p,int *q)
+{
+    swap(*p,*q);
+}
+int main()
+{
+     int a,b;
+     int *p=&a;
+     int *q=&b;
+     cout<<"podaj a,b"<<endl;
+     cin>>a;
+     cin>>b;
+     funckja(p,q);
+     cout<<"a="<<a<<endl;
+     cout<<"b="<<b<<endl;
+}
