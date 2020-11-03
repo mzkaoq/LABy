165f34bea17d07faf88f309ff7f12e93d9209110
mzkaoq <mzkaoq333@gmail.com>
2020-11-03 23:34:24 +0100

    zad3

---
+++ laby2/zad3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void funckja(int &p,int *q)
+{
+     swap(p,*q);
+}
+int main()
+{
+     int a,b;
+     cout<<"podaj a,b"<<endl;
+     cin>>a;
+     cin>>b;
+     funckja(a,&b);
+     cout<<"a="<<a<<endl;
+     cout<<"b="<<b<<endl;
+}
