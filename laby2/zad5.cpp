39c79e384a8562363d37dddee841f783ad28871a
mzkaoq <mzkaoq333@gmail.com>
2020-11-04 00:36:05 +0100

    dodanie testu

--- laby2/zad5.cpp
+++ laby2/zad5.cpp
@@ -9,12 +9,19 @@ void funckja(int *p,int *q)
 int main()
 {
      int a,b;
+     int c,d;
      int *p=&a;
      int *q=&b;
      cout<<"podaj a,b"<<endl;
      cin>>a;
      cin>>b;
+     c=a;
+     d=b;
      funckja(p,q);
      cout<<"a="<<a<<endl;
      cout<<"b="<<b<<endl;
+    if(c==b&&d==a)
+    {
+        cout<<"test zdany pomyslnie";
+    }
 }
