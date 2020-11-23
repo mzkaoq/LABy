3a80db8113775175a19cadc982cdac104ecdccb7
mzkaoq <mzkaoq333@gmail.com>
2020-11-23 23:13:51 +0100

    Matrix h

---
+++ laby3/Matrix.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Matrix
+{
+    public:
+    //konstruktory
+    Macierz(int x,int y);
+    Macierz(int x);
+    //metody
+    void Print();
+};
