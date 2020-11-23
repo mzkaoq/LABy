5d8c95e07de72de9a1d6c1c53d9fed63b6638d39
mzkaoq <mzkaoq333@gmail.com>
2020-11-23 23:12:57 +0100

    podstawy

---
+++ laby3/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Matrix.cpp"
+
+using namespace std;
+
+
+void test()
+{
+    Matrix macierz(5,6);
+    macierz.Print();
+}
+int main()
+{
+    test();
+    return 0;
+}
