0ba2e917df88e6b98825d775169f02fbf0d96e20
mzkaoq <mzkaoq333@gmail.com>
2020-11-28 22:46:31 +0100

    Laby 3 ostateczna wersja

--- laby3/Matrix.cpp
+++ laby3/Matrix.cpp
@@ -53,13 +53,148 @@ Matrix::Matrix(int x) : rze(x), kol(x)
 
 void Matrix::Print()
 {
-    cout << endl;
     for (int i=0; i<rze; i++)
     {
-    for (int j=0; j<kol; j++)
-    {
-    cout << tab[i][j] << " ";
+        for (int j=0; j<kol; j++)
+        {
+          cout << tab[i][j] << " ";
+        }
+        cout << endl;
     }
     cout << endl;
+}
+void Matrix::Set(int n, int m, double val)
+{
+    tab[n][m]=val;
+}
+double Matrix::Get(int n, int m)
+{
+    cout<<tab[n][m];
+    cout<< endl;
+    return tab[n][m];
+}
+Matrix Matrix::Add(Matrix m2)
+{
+    if(this->rze!=m2.rze || this->kol!=m2.kol)
+    {
+        cout << "nie mozna dodac macierzy"<<endl;
+        exit(1);
+    }
+    Matrix m3(rze,kol);
+     for (int i=0; i<m3.rze; i++)
+    {            
+        for (int j=0; j<m3.kol; j++)
+        {   
+            m3.tab[i][j]=this->tab[i][j]+m2.tab[i][j];
+        }
+    }
+    return m3;
+}
+Matrix Matrix::Sub(Matrix m2)
+{
+    if(this->rze!=m2.rze || this->kol!=m2.kol)
+    {
+        cout << "nie mozna odjac macierzy"<<endl;
+        exit(1);
+    }
+    Matrix m3(rze,kol);
+     for (int i=0; i<m3.rze; i++)
+    {            
+        for (int j=0; j<m3.kol; j++)
+        {   
+            m3.tab[i][j]=this->tab[i][j]-m2.tab[i][j];
+        }
+    }
+    return m3;
+}
+Matrix Matrix::Multiply(Matrix m2)
+{
+    int k=0;
+    if(this->rze!=m2.kol)
+    {
+        cout << "nie mozna przemnozyc macierzy"<<endl;
+        exit(1);
+    }
+    Matrix m3(this->rze,m2.kol);
+    double suma=0;
+     for (int i=0; i<m2.kol; i++)
+    {            
+        for (int j=0; j<m3.rze; j++)
+        {   
+            for(k=0; k<m3.kol;k++)
+            {
+                suma+=this->tab[j][k]*m2.tab[k][i];
+            }
+            m3.tab[j][i]=suma;
+            suma=0;
+        }
+    }
+    return m3;
+}
+
+int Matrix::cols()
+{
+    return kol;
+}
+
+int Matrix::rows()
+{
+    return rze;
+}
+
+void Matrix::store(string filename, string path)
+{
+    string fullpath = path + '/' + filename;
+    ofstream File;
+    File.open(fullpath);
+    if (File.is_open())
+    {
+        File << kol << " " << rze << endl;
+        for (int i = 0; i < kol; i++)
+        {
+            for (int j = 0; j < rze; j++)
+            File << tab[i][j] << " ";
+        
+        File << endl;
+        }
+        File.close();
+    }
+    else 
+    {
+        cout<<"blad otwarcia"<<endl;
+        exit(3);
+    }
+}
+
+Matrix::Matrix(string fullpath)
+{
+    ifstream File;
+    File.open(fullpath);
+     if (File.is_open())
+    {
+        File>>kol;
+        File>>rze;
+
+        if(kol>0 && rze>0)
+        {
+            tab = new double*[rze];
+            for (int i = 0; i < rze; ++i)
+            {
+                tab[i] = new double[kol];
+            }
+            for (int i = 0; i < rze; ++i) 
+            {
+                for (int j = 0; j < kol; ++j)    
+                {
+                    File>>tab[i][j];
+                }
+            }
+        }
+        File.close();
+    }
+    else 
+    {
+        cout<<"blad otwarcia"<<endl;
+        exit(3);
     }
 }
--- laby3/Matrix.h
+++ laby3/Matrix.h
@@ -11,6 +11,15 @@ class Matrix
     //konstruktory
     Matrix(int x,int y);
     Matrix(int x);
+    Matrix(string fullpath);
     //metody
     void Print();
+    void Set(int n, int m, double val);
+    double  Get(int n, int m);
+    Matrix Add(Matrix m2);
+    Matrix Sub(Matrix m2);
+    Matrix Multiply(Matrix m2);
+    int cols();
+    int rows();
+    void store(string filename, string path);
 };
