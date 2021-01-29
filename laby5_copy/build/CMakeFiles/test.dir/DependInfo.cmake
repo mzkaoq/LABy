904b2be6612eb56e1d39c10285da296e241d12ce
Filip Bulinski <72728316+mzkaoq@users.noreply.github.com>
2021-01-29 12:43:20 +0100

    Add files via upload
    sqlite3

---
+++ laby5_copy/build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/THINK/Desktop/C++/LABy/laby5_copy/libs/sqlite3/sqlite3.c" "C:/Users/THINK/Desktop/C++/LABy/laby5_copy/build/CMakeFiles/test.dir/libs/sqlite3/sqlite3.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "../test"
+  "../include"
+  "../libs"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/THINK/Desktop/C++/LABy/laby5_copy/src/Matrix.cpp" "C:/Users/THINK/Desktop/C++/LABy/laby5_copy/build/CMakeFiles/test.dir/src/Matrix.cpp.obj"
+  "C:/Users/THINK/Desktop/C++/LABy/laby5_copy/src/main.cpp" "C:/Users/THINK/Desktop/C++/LABy/laby5_copy/build/CMakeFiles/test.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../test"
+  "../include"
+  "../libs"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ laby5_copy/build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/libs/sqlite3/sqlite3.c.obj"
+  "CMakeFiles/test.dir/src/Matrix.cpp.obj"
+  "CMakeFiles/test.dir/src/main.cpp.obj"
+  "libtest.dll.a"
+  "test.exe"
+  "test.exe.manifest"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
